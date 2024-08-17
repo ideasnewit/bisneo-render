c88cdfffeb5d96c4a90df2d5987d9bba6d933907
ideasnewit <132282145+ideasnewit@users.noreply.github.com>
2024-08-17 13:23:09 +0530

    Delete dist/api directory

--- dist/api/routes/account.js
+++
@@ -1,8 +0,0 @@
-import { default as express } from "express";
-import * as accountController from "../controllers/account.js";
-import { userRules } from "../../middlewares/validators/rules/user.js";
-import { simpleValidate } from "../../middlewares/validators/validate.js";
-export const router = express.Router();
-router
-    .route("/login")
-    .post(userRules.login, simpleValidate, accountController.login);
